a9f27aacb9d7ef23897fdadc13e83a6ce67d6857
LeonHecht <l-e-o-n-h@web.de>
2025-01-02 13:42:10 -0600

    evaluation/inference for bge-m3 model

--- evaluation.py
+++ evaluation.py
@@ -4,15 +4,97 @@ from tqdm import tqdm
 import pickle
 import os
 import pytrec_eval
+import numpy as np
+
+# Globals
+MAX_QUERY_LEN = 256
+MAX_DOC_LEN = 2048
 
 
-def load_model(checkpoint):
-    return model_setup.load_model(checkpoint)
+def get_bge_m3_model(checkpoint):
+    """ Load BAAI embeddings model."""
+    from FlagEmbedding import BGEM3FlagModel
+
+    # model = BGEM3FlagModel(checkpoint, use_fp16=True) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+    model = BGEM3FlagModel(checkpoint) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+    return model
 
 
 def load_data(country):
     ds = dataset_preparation.load_messirve_dataset(country)
     return ds
+
+
+def retrieve(query_ids, doc_ids, embeddings_queries, embeddings_docs, sim_type='dot'):
+    """
+    Given a list of queries and documents, and their embeddings, compute the similarity between queries and documents
+    and return the top_k most similar documents for each query.
+    Dot-product and cosine similarity are supported.
+
+    Args:
+        queries (list): List of queries.
+        docs (list): List of documents.
+        query_ids (list): List of query IDs.
+        doc_ids (list): List of document IDs (Codigos).
+        embeddings_queries (np.ndarray): Embeddings of queries.
+        embeddings_docs (np.ndarray): Embeddings of documents.
+        top_k (int): Number of most similar documents to retrieve.
+        sim_type (str): Type of similarity to use. Options: 'dot', 'cosine'.
+    
+    Returns:
+        dict: Dictionary with query_id as key and a dict of {doc_id: similarity, ...} as value
+        (run format for pytrec_eval library).
+    """
+    import torch.nn.functional as F
+    
+    if sim_type == 'dot':
+        similarity = embeddings_queries @ embeddings_docs.T
+    elif sim_type == 'cosine':
+        # Only use angle between embeddings
+        embeddings_queries = F.normalize(embeddings_queries, p=2, dim=1)
+        embeddings_docs = F.normalize(embeddings_docs, p=2, dim=1)
+        similarity = (embeddings_queries @ embeddings_docs.T) * 100
+    else:
+        raise ValueError(f"Invalid similarity type: {sim_type}")
+    # print("similarity", similarity)   # [[0.6265, 0.3477], [0.3499, 0.678 ]]
+    
+    run = {}
+    for i in range(len(query_ids)):
+        query_sim = similarity[i]
+        
+        run[str(query_ids[i])] = {str(doc_ids[j]): float(query_sim[j]) for j in range(len(doc_ids))}
+
+    return run
+
+
+def embed_bge(model, docs, queries, doc_ids, query_ids):
+    """
+    Embed the queries and documents using the BAAI embeddings models and compute the similarity between queries and documents.
+    Calls the retrieve function.
+
+    Args:
+        model: BAAI embeddings model.
+        docs (dict): Dictionary with document_id as key and text as value.
+        queries (list): List of queries.
+        top_k (int): Number of most similar documents to retrieve.
+
+    Returns:   
+        dict: Dictionary with query as key and a list of tuples of (similarity, document text, doc_id) as value.
+    """
+    embeddings_queries = model.encode(queries, batch_size=8, max_length=MAX_QUERY_LEN)['dense_vecs']
+    # Embed entire corpus if file does not exist
+    path = 'corpus/embeddings_corpus_bge-m3.npy'
+    if not os.path.exists(path):
+        embeddings_docs = model.encode(docs, batch_size=8, max_length=MAX_DOC_LEN)['dense_vecs']    # takes about 7min
+        # save embeddings
+        np.save(path, embeddings_docs)
+    else:
+        # Load embeddings
+        embeddings_docs = np.load(path)
+
+    # Compute similarities
+    run = retrieve(query_ids, doc_ids, embeddings_queries, embeddings_docs)
+    return run
 
 
 def retrieve_bm25(docs, queries, doc_ids, query_ids):
@@ -75,6 +157,21 @@ def run(model, metrics, country):
             with open(run_path, "rb") as f:
                 run = pickle.load(f)
         print("BM25 retrieval done.")
+    elif model == "bge":
+        run_path = f"run_bge_{country}.pkl"
+        if not os.path.exists(run_path):
+            checkpoint = 'BAAI/bge-m3'
+            # checkpoint = 'BAAI/bge-m3-unsupervised'
+            # checkpoint = 'BAAI/bge-m3-retromae'
+            model = get_bge_m3_model(checkpoint)
+            run = embed_bge(model, docs, queries, doc_ids, query_ids)
+            # save run to disk using pickle
+            with open(run_path, "wb") as f:
+                print("Dumping run to pickle file...")
+                pickle.dump(run, f)
+        else:
+            with open(run_path, "rb") as f:
+                run = pickle.load(f)
     else:
         raise ValueError("Model not supported.")
     
@@ -110,4 +207,5 @@ def run(model, metrics, country):
 
 
 if __name__ == "__main__":
-    run(model="bm25", metrics={'ndcg', 'ndcg_cut.10', 'recall_100', 'recip_rank'}, country="ar")
+    # run(model="bm25", metrics={'ndcg', 'ndcg_cut.10', 'recall_100', 'recip_rank'}, country="ar")
+    run(model="bge", metrics={'ndcg', 'ndcg_cut.10', 'recall_100', 'recip_rank'}, country="ar")
